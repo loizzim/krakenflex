a1857dc787086bc64602ee26a10d85137195c755
Marianna Loizzi <loizzi.ma@gmail.com>
2023-10-24 12:36:37 +0100

    updating the class file with main and retry

--- class_endpoint.py
+++ class_endpoint.py
@@ -1,25 +1,60 @@
 import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 class Kraken:
     def __init__(self):
         self.base_url = "https://api.krakenflex.systems/interview-tests-mock-api/v1"
-        self.headers = {"x-api-key": "EltgJ5G8m44IzwE6UN2Y4B4NjPW77Zk6FJK3lL23"} 
+        self.headers = {"x-api-key": "EltgJ5G8m44IzwE6UN2Y4B4NjPW77Zk6FJK3lL23"}
+        session = requests.Session()
+
+        retries = Retry(total = 3, status_forcelist = [500])
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        self.mySession = session
+
+
+    def main(self):
+        # Get outages
+        outages_response = self.invoke_get("outages")
+
+        #retry invoke.get
+        outages = self.handle_response(outages_response)
+        
+        # Get device information
+        device_info_response = self.invoke_get("site-info/norwich-pear-tree")
+
+        #retry invoke.get
+        device_info = self.handle_response(device_info_response)
+
+        # Extract device IDs from device info
+        device_ids = self.extract_device_id(device_info)
+
+        # Filter outages based on IDs and begin time
+        filtered_data = self.filter_outages(outages, device_ids)
+
+        # Enrich outages with names from the IDs list
+        enriched_outages = self.outages_with_name(filtered_data, device_info.get("devices"))
+
+        # Post enriched outages
+        post_response = self.invoke_post("site-outages/norwich-pear-tree", enriched_outages)
+
+        # invoke 
+        self.handle_response(post_response)
 
 
     def invoke_get(self, endpoint):
-        outages_response = requests.get(f"{self.base_url}/{endpoint}", headers=self.headers)
-        if outages_response.status_code == 200:
-            return outages_response.json()
-        else:
-            print("Request failed with status code:", outages_response.status_code)
-            return None  # Return None or raise an exception in case of failure
+        return self.mySession.get(f"{self.base_url}/{endpoint}", headers=self.headers)
 
     def invoke_post(self, endpoint, data):
-        post_response = requests.post(f"{self.base_url}/{endpoint}", headers=self.headers, json=data)
-        if post_response.status_code == 200:
-           print("Request was successful")
+        return self.mySession.post(f"{self.base_url}/{endpoint}", headers=self.headers, json=data)
+
+    def handle_response(self, response):
+        if response.status_code == 200:
+            print("Request successful", response.url)
+            return response.json()
         else:
-            print("Request failed with status code:", post_response.status_code)
+            print("Request failed with status code:", response.status_code,"endpoint:", response.url, "message:", response.json()["message"] )
+            return None
 
     def extract_device_id(self, device_info):
    #  info= invoke_get("site-info/norwich-pear-tree")
@@ -56,5 +91,5 @@ class Kraken:
                     enriched_outages.append(result)
         return enriched_outages
 
-
-        
+if __name__ == "__main__":
+    Kraken().main()
--- kraken_main_class.py
+++ kraken_main_class.py
@@ -1,26 +1,3 @@
-from class_endpoint import Kraken
-
-# initialise an instance of the class Kraken
-instance = Kraken()
-
-#call outages and get_device info  methods to get respectively the outages and devices lists
-
-outages = instance.invoke_get("outages")
-get_devices= instance.invoke_get("site-info/norwich-pear-tree")
-
-
-# call the extract_device_id method to extract the ids from the device info list 
-device_ids =instance.extract_device_id(get_devices)
-
-
-
-# filter the outages list based on the begin date and if the ids is present in the devices id list 
-filtered_data = instance.filter_outages(outages,device_ids)
-
-# enrich the filtered outages list with the name object from the devices id info, based on common ids
-instance.outages_with_name(filtered_data, get_devices["devices"])
-
-# call the invoke post method to post the enriched outages list 
-post = instance.invoke_post("site-outages/norwich-pear-tree",filtered_data)
-
-
+# from class_endpoint import Kraken
+# # initialise an instance of the class Kraken
+# Kraken().main()
